7e8bfde1e1ebdb578577480f72663ce5166d2ff1
LeeJongHun <epilogueljh@gmail.com>
2026-07-07 09:17:44 +0900

    Delete code except project code

--- 2026-07-02_backup/gazebo_simulation/copy_files.sh
+++
@@ -1,4 +0,0 @@
-sudo cp my_world.launch.py	/opt/ros/humble/share/turtlebot3_gazebo/launch
-sudo cp my_world.world		/opt/ros/humble/share/turtlebot3_gazebo/worlds
-sudo cp -r materials		/opt/ros/humble/share/turtlebot3_gazebo/
-sudo cp project_map.*		~/
--- second_project_pkg/copy_files.sh
+++
@@ -1,2 +0,0 @@
-sudo cp setup.py		/home/user/ros2_ws/src/second_project_pkg
-sudo cp ./second_project_pkg/*	/home/user/ros2_ws/src/second_project_pkg/second_project_pkg
